f53b3c5167267cdc5ee27fbedbfb310689986615
Martin Kopp <Hraciemail128@seznam.cz>
2025-12-14 10:56:26 +0100

    Verze 1.1 - přidání komentářů, ošetření vyjímek try / except

--- task_manager.py
+++ task_manager.py
@@ -2,13 +2,25 @@
 ukoly = []
 
 def pridat_ukol():
+    #Funkce pridat_ukol přidá nový úkol do seznamu úkolů s názvem a popisem.
+
     global ukoly
-    ukol_nazev = str(input("\nZadej název úkolu: "))
-    ukol_popis = str(input("Zadej popis úkolu: "))
+    ukol_nazev = input("\nZadej název úkolu: ").strip()
+    if ukol_nazev == "":
+       print("Název úkolu nemůže být prázdný.")
+       return
+
+    ukol_popis = input("Zadej popis úkolu: ").strip()
+    if ukol_popis == "":
+       print("Popis úkolu nemůže být prázdný.")
+       return
+
     ukoly.append({"nazev": ukol_nazev, "popis": ukol_popis})
     print(f"Úkol '{ukol_nazev}' byl přidán.")
 
 def zobrazit_ukoly():
+    #Funkce zobrazit_ukoly zobrazí všechny úkoly v seznamu s jejich názvy a popisy.
+
     if not ukoly:
         print("\nSeznam úkolu je prázdný.")
         return
@@ -19,6 +31,8 @@ def zobrazit_ukoly():
         print(f"{i}. {ukol['nazev']} - {ukol['popis']}")
 
 def odstranit_ukoly():
+    #Funkce odstranit_ukoly odstraní úkol ze seznamu na základě zadaného čísla úkolu.
+
         if not ukoly:
             print("\nSeznam úkolu je prázdný.")
             return
@@ -37,7 +51,11 @@ def odstranit_ukoly():
               print("Musíš zadat číslo.")
 
 def hlavni_menu():
+    #Funkce hlavni_menu zobrazí hlavní menu a umožní uživateli vybrat akce pro správu úkolů.
+
     while True:
+       try:
+
         volba = int(input(  " \n"
                 "Správce úkolů - Hlavní menu\n"
                 "1. Přidat nový úkol\n"
@@ -59,6 +77,9 @@ def hlavni_menu():
                 break
             case _:
                 print("Neplatná volba.")
+
+       except ValueError:
+              print("\nMusíš zadat číslo.")
 
 
 hlavni_menu()
